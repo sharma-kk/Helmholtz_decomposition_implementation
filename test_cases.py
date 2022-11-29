4ecd1ada1a30fca0f7db334edeced739984c6234
sharma-kk <sharma.uhh@gmail.com>
2022-11-29 17:58:14 +0100

    added explosion-like test case

--- test_cases.py
+++ test_cases.py
@@ -16,7 +16,8 @@ x,y = SpatialCoordinate(mesh)
 
 print("option 1: Wind blowing over the ocean from left to right in square patch of the domain \n")
 print("option 2: Circular velocity field of the atmosphere like a cyclone rotating anti-clockwise \n")
-opt = input("Pleace choose from the above options (type 1 or 2) for atm vel field: \n")
+print("option 3: explosion, the air is moving outwards")
+opt = input("Pleace choose from the above options (type 1, 2, or 3) for atm vel field: \n")
 
 circ = conditional(sqrt(pow(x-0.5, 2) + pow(y-0.5,2)) < 0.25, 1.0, 0.0)
 sq = conditional(And(And(x> 0.25, x < 0.75), And(y > 0.25, y < 0.75)), 1.0, 0.0)
@@ -28,9 +29,11 @@ if opt=="1":
 elif opt=="2":
     u.assign(project(as_vector([-circ*(y-0.5), circ*(x-0.5)]), W))
     o_file = "test_case_cyclone.pvd"
+elif opt == "3":
+    u.assign(project(as_vector([circ*(x-0.5), circ*(y-0.5)]), W))
+    o_file = "test_case_explosion.pvd"
 else:
     print("wrong choice ! program won't run successfully !")
-
 
 n = FacetNormal(mesh)
 
